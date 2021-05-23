af442695f9fac2a8e32965d6f0bafb407e620c57
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-23 14:12:53 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports.run = async(client, message, args) => {
     const embed = await message.channel.send(` \`🏓 Pong!\` `);
     embed.edit(`
-  \`💻 Ping do servidor:\` \`${ferinha.createdTimestamp -
+  \`💻 Ping do servidor:\` \`${embed.createdTimestamp -
         message.createdTimestamp}ms\`
   \`⏰ Ping da API:\` \`${Math.round(
         client.ws.ping
